9dc3bb669a12c57b9eae8fe008982c16e5e0e711
PhucKhang1310 <npkhang2005@gmail.com>
2025-09-11 08:50:46 +0700

    feat: Switch from BrowserRouter to HashRouter; update navigation paths in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,30 @@
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
-import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom";
+import { HashRouter, Route, Routes, useNavigate } from "react-router-dom";
 import HomePage from "./components/HomePage";
 import UnderConstruction from "./components/UnderConstruction";
 
 function NavBarWrapper() {
   const navigate = useNavigate();
   const onChange = (key: string) => {
-    navigate(`portfolio/${key.toLowerCase()}`);
+    navigate(`/${key.toLowerCase()}`);
   };
   return <NavBar onChange={onChange} />;
 }
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <div>
         <NavBarWrapper />
         <Routes>
-          <Route path="/portfolio" element={<HomePage />} />
-          <Route path="/portfolio/home" element={<HomePage />} />
-          <Route path="/portfolio/contact" element={<UnderConstruction />} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/contact" element={<UnderConstruction />} />
         </Routes>
         <Footer />
       </div>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
